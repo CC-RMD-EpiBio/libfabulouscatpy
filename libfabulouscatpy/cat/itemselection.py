fix to kl crit

libfabulouscatpy/cat/itemselection.py
@@ -84,12 +84,13 @@ class ItemSelector(ABC):
         temperature: float = 0.01,
         randomize_items: bool = True,
         randomize_scales: bool = True,
-        unscored_freq: float = 0.33,
-        precision_limit: float = 0.3873,
+        unscored_freq: float = 0.0,
+        precision_limit: float = 0.33333, #0.303, #0.3873,
         min_responses: int = 5,
         max_responses: int = 12,
         inadmissable_scales: list[str] | None = None,
         deterministic: bool = True,
+        omit_items: list[str] | None = None,
         **kwargs,
     ) -> None:
         """Instantiate item selector class
@@ -110,6 +111,7 @@ class ItemSelector(ABC):
         self.randomize_scales = randomize_scales
         self.unscored_freq = unscored_freq
         self.temperature = temperature
+        self.omit_items = omit_items if omit_items is not None else []
         self.items = items
         self.scales = scales
         self.itemdict = {x["item"]: x for x in items}
@@ -137,6 +139,7 @@ class ItemSelector(ABC):
 
         answered = tracker.responses.keys()
         un = [i for i in self.items if i["item"] not in answered]
+        un = [i for i in un if i["item"] not in self.omit_items]
         if scale is not None:
             un = [i for i in un if "scales" in i.keys()]
             un = [i for i in un if scale in i["scales"].keys()]
@@ -262,6 +265,7 @@ class ItemSelector(ABC):
             if candidate is not None:
                 self.unscored_count += 1
                 return candidate
+            
         return self._next_scored_item(tracker, scale)
     
     @abstractmethod
@@ -291,7 +295,7 @@ class ItemSelector(ABC):
         variance = list(criterion.values())
         
         variance /= np.max(variance)
-        probs = np.exp(-variance) ** (1 / self.temperature)
+        probs = np.exp(-variance / self.temperature)
         probs /= np.sum(probs)
 
         if self.deterministic:
@@ -356,6 +360,7 @@ class ItemSelector(ABC):
                 session.errors[proposed_scale] is not None
                 and (session.errors[proposed_scale] < self.PRECISION_LIMIT)
                 and unskipped >= self.PRECISION_RESPONSE_MINIMUM
+                and unskipped <= self.max_responses
             ):
                 # print(f"closing scale: {proposed_scale} with {unskipped} responses", file=sys.stderr)
 

libfabulouscatpy/cat/itemselectors/kl.py
@@ -100,14 +100,16 @@ class KLItemSelector(ItemSelector):
         )[
             :, unresponded_ndx, :
         ]  # ll for unobserved items
+        # N_grid x N_item x K
         p_itemized = np.exp(lp_itemized)
         pi_density = scoring.scores[scale].density
         
         lp_infty = lp_itemized + energy[:, np.newaxis, np.newaxis]
+        # N_grid x N_item x K
         pi_infty = np.exp(lp_infty - np.max(lp_infty, axis=0, keepdims=True))
         pi_infty /= np.trapz(
             y=pi_infty, x=scoring.interpolation_pts[scale], axis=0
-        )
+        ) # N_grid x N_item x K
         ##########
         # $\pi_\infty$ is computed
         ########
@@ -117,9 +119,9 @@ class KLItemSelector(ItemSelector):
             x=self.scoring.interpolation_pts[scale],
             axis=0,
         )  # p_{ik}^{\alpha_t}
-
+        expected_pi_infty = np.sum(pi_infty * p_itemized, axis=-1, keepdims=True)
         A = np.trapz(
-            y=pi_infty * lp_itemized,
+            y=expected_pi_infty * lp_itemized, # pi_infty
             x=scoring.interpolation_pts[scale],
             axis=0,
         )  # This is an integral over \theta, A will have shape I x K
@@ -165,11 +167,11 @@ class KLItemSelector(ItemSelector):
                 Delta += [v]
         if len(items) == 0:
             return {}
-        Delta -= np.max(Delta)
+        Delta -= np.min(Delta)
         probs = np.exp(-Delta/self.temperature)
         probs /= np.sum(probs)
     
-        if self.deterministic or (self.hybrid  and  ((self.scoring.n_scored[scale] > 1))):
+        if self.deterministic or (self.hybrid  and  ((self.scoring.n_scored[scale] > 3))):
             ndx = np.argmax(probs)
         else:
             ndx = np.random.choice(np.arange(len(criterion.keys())), p=probs)
@@ -180,6 +182,51 @@ class KLItemSelector(ItemSelector):
         return {}
 
 
+class NegativeKLItemSelector(KLItemSelector):
+    description = """Greedy anti plugin KL selector"""
+
+    def _next_scored_item(
+        
+        self, tracker: CatSessionTracker, scale=None
+        ) -> dict[str : dict[str:Any]]:
+        
+        scale = self.next_scale(tracker)
+        un_items = self.un_items(tracker, scale)
+
+        if un_items is None:
+            # Not sure if this can happen under normal testing, but included as
+            # a safety feature.
+            return None
+
+        trait = tracker.scores[scale]
+        trait = 0.0 if trait is None else trait
+        error = tracker.errors[scale]
+        error = 100.0 if error is None else error
+        
+        criterion = self.criterion(scoring=self.scoring, items = un_items, scale=scale)
+        valid_items = [x['item'] for x in un_items]
+        items = []
+        Delta = []
+        for k, v in criterion.items():
+            if k in valid_items:
+                items += [k]
+                Delta += [v]
+        if len(items) == 0:
+            return {}
+        Delta -= np.max(Delta)
+        probs = np.exp(Delta/self.temperature)
+        probs /= np.sum(probs)
+    
+        if self.deterministic or (self.hybrid  and  ((self.scoring.n_scored[scale] > 3))):
+            ndx = np.argmax(probs)
+        else:
+            ndx = np.random.choice(np.arange(len(criterion.keys())), p=probs)
+        result = list(criterion.keys())[ndx]
+        for i in un_items:
+            if i['item'] == result:
+                return i
+        return {}
+    
 class StochasticKLItemSelector(KLItemSelector):
     description = "Stochastic KL selector"
 